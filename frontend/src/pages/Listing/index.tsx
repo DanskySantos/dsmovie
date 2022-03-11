20d6c03b1c5a9197c8c48b946fe6f6d6edf69d2b
Dansky Santos <dansky123@hotmail.com>
2022-03-11 13:55:59 -0300

    show Score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -23,7 +23,7 @@ function Listing() {
 
     useEffect(()=>{
         //https://dansky-dsmovie.herokuapp.com/movies?size=12&page=0
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
